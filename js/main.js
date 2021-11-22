d793643baf75ec0e990205f0071ff2ace782705b
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-11-22 15:21:14 +0400

    создание слайдера

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	autoHeight: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button__next',
+		prevEl: '.slider-button__prev',
+	},
+});
