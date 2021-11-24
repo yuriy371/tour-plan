9f41bfe9f0e7fe86868a7abeb2e8edbd18e77f34
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-11-24 23:44:41 +0400

    фикс слайдера

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ const hotelSlider = new Swiper('.hotel-slider', {
 const reviewsSlider = new Swiper('.reviews-slider', {
 	// Optional parameters
 	loop: true,
-	autoHeight: true,
 	// Navigation arrows
 	navigation: {
 		nextEl: '.reviews-button__next',
