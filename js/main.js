521a41ea3dff73f541dd6eae18398e9b06ee3468
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-11-30 10:37:15 +0400

    создание мобильного меню

--- js/main.js
+++ js/main.js
@@ -10,9 +10,11 @@ const hotelSlider = new Swiper('.hotel-slider', {
 		enabled: true,
 	}
 });
+
 const reviewsSlider = new Swiper('.reviews-slider', {
 	// Optional parameters
 	loop: true,
+	autoHeight: true,
 	// Navigation arrows
 	navigation: {
 		nextEl: '.reviews-button__next',
@@ -20,6 +22,13 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 	},
 });
 
-$('.newsletter').parallax({ imageSrc: './img/bg-news.jpg' });
+let menuButton = document.querySelector(".menu-button")
+
+menuButton.addEventListener("click", () => {
+	console.log(15);
+	document.querySelector(".header-menu").classList.toggle("header-menu__visible")
+})
+
+$('.newsletter').parallax({ imageSrc: '/img/bg-news.jpg' });
 
 // document.body.contentEditable = true
