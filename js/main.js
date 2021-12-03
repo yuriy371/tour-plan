585b9cb1d61ffc2be65b7445582d786f9e219ae8
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-12-03 12:08:30 +0400

    создание модального окна

--- js/main.js
+++ js/main.js
@@ -23,11 +23,41 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 });
 
 let menuButton = document.querySelector(".menu-button")
+let modalBtn = $("[data-toggle=modal]")
+let closeModalBtn = $(".modal__close")
 
+let modalOverlay = $(".modal__overlay")
+let modalDialog = $(".modal__dialog")
+
+let openModal = () => {
+	modalOverlay.addClass("modal__overlay--visible")
+	modalDialog.addClass("modal__dialog--visible")
+	$("body").addClass("body__hidden")
+}
+
+let closeModal = (e) => {
+	e.preventDefault()
+	modalOverlay.removeClass("modal__overlay--visible")
+	modalDialog.removeClass("modal__dialog--visible")
+	$("body").removeClass("body__hidden")
+
+}
+
+$(document).on('keydown', (e) => {
+	if (e.which === 27) { // key = esc (27)
+		modalOverlay.removeClass("modal__overlay--visible")
+		modalDialog.removeClass("modal__dialog--visible")
+		$("body").removeClass("body__hidden")
+		return false;
+	}
+});
 menuButton.addEventListener("click", () => {
 	document.querySelector(".header-menu").classList.toggle("header-menu__visible")
 	document.querySelector("body").classList.toggle("body__hidden")
 })
+
+modalBtn.on("click", openModal)
+closeModalBtn.on("click", closeModal)
 
 $('.newsletter').parallax({ imageSrc: '/img/bg-news.jpg' });
 
