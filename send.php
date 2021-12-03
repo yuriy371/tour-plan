d4efcae5183e47ad03f20261857568d823aac3d9
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-12-03 12:56:31 +0400

    создание модального окна

--- send.php
+++ send.php
@@ -11,7 +11,16 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 // Формирование самого письма
-if ($email) {
+if ($email && $phone) {
+    $title = "Запись в отель Best Tour Plan";
+    $body = "
+    <h2>Новое обращение</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон:</b> $phone<br><br>
+    <b>Почта:</b> $email<br><br>
+    <b>Сообщение:</b><br>$message
+    ";
+} elseif ($email) {
     $title = "Подписка на новости Best Tour Plan";
     $body = "
     <h2>Подписка на новости</h2>
@@ -31,11 +40,13 @@ if ($email) {
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
-    $mail->isSMTP();   
+    $mail->isSMTP();
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
     // $mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+    $mail->Debugoutput = function ($str, $level) {
+        $GLOBALS['status'][] = $str;
+    };
 
     // Настройки вашей почты
     $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
@@ -46,24 +57,26 @@ try {
     $mail->setFrom('sfomenko612@mail.ru', 'Сергей Фоменко'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('anzelafranc@mail.ru');  
+    $mail->addAddress('anzelafranc@mail.ru');
 
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
 
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {
+        $result = "success";
+    } else {
+        $result = "error";
+    }
 } catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
 
 // Отображение результата
-if ($email) {
+if (!$phone) {
     header('Location: subscribe.html');
 } else {
     header('Location: thankyou.html');
